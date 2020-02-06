b1d2236f01cdd01e7281589740a5b26149bd8544
Jonas Koponen <jonas.koponen@sok.fi>
2020-02-06 08:58:05 +0200

    Simplify script (I.M)

--- ad-aws-login.sh
+++ ad-aws-login.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
-
-PROFILE_NAME=
-APP_NAME=
-DURATION_HOURS=4
-ROLE_ARN=
-
-readonly AWS_CONFIG="${HOME}/.aws/config"
-readonly AWS_CREDENTIALS=~/.aws/credentials
-readonly THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
-readonly EXTENSION="$THIS_DIR/chrome_extension"
-readonly USER_DATA_DIR="$THIS_DIR/user_data"
-readonly TEMP_FILE="${HOME}/Downloads/temporary_aws_credentials$(date +"%Y-%m-%d_%H-%M-%S").txt"
-
+cd "$(dirname "$0")"
 function usage() {
     cat <<EOF
 Usage: ${0} [OPTIONS]
-
   Simple script that fetches temporary AWS credentials with Azude AD login
   (https://myapps.microsoft.com).
-
 Options:
   --profile  TEXT    The name of the profile in ~/.aws/credentials to update.
   --app      TEXT    A substring of the app name shown in myapps.microsoft.com
@@ -30,96 +15,40 @@ Options:
 EOF
     exit 128
 }
-
-function err() {
-    echo "$@" >&2
-}
-
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -p|--profile)
-            PROFILE_NAME=$2
-            shift
-            shift
-            ;;
-        -a|--app)
-            APP_NAME=$2
-            shift
-            shift
-            ;;
-        -d|--duration)
-            DURATION_HOURS=$2
-            shift
-            shift
-            ;;
-        -r|--role-arn)
-            ROLE_ARN=$2
-            shift
-            shift
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-
-function selaws() {
-    select _profile in $(cat "${AWS_CONFIG}" | grep '\[profile' | sed 's/\[profile \(.*\)]/\1/'); do
-        echo $_profile
-        break
-    done
-}
-
-# Check that AWS config and selected profile exists.
-if [ ! -f "${AWS_CONFIG}" ]; then
-    err "AWS config file (${AWS_CONFIG}) does not exist. Cannot continue."
-    exit 1
-fi
-
-if [[ -z $PROFILE_NAME ]]; then
-    echo "No AWS profile given, select manually:"
-    PROFILE_NAME=$(selaws)
-fi
-
-if ! cat "${AWS_CONFIG}" | grep -q "^\[profile ${PROFILE_NAME}\]$"; then
-    err "Profile ${PROFILE_NAME} not found in ${AWS_CONFIG}."
-    exit 2
-fi
-
-
-rm -f $TEMP_FILE
-
-# if chrome is already open, we would get just new tab without our extensions,
-# unless we use custom --user-data-dir
-mkdir -p "$USER_DATA_DIR"
-
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
-    --load-extension="$EXTENSION" --disable-extensions-except="$EXTENSION" \
-    --user-data-dir="$USER_DATA_DIR" \
-    'http://localhost/?durationHours='$DURATION_HOURS'&app='$APP_NAME'&filename='$(basename ${TEMP_FILE})'&roleArn='$ROLE_ARN 2>/dev/null &
-
-while [ ! -f $TEMP_FILE ]; do
-  sleep 1
-done
-
-
-if [ -e $AWS_CREDENTIALS ]; then
-  cp $AWS_CREDENTIALS $AWS_CREDENTIALS.bak
-  # delete section with old values, if any
-  awk '/^\[/{keep=1} /^\['$PROFILE_NAME'\]/{keep=0} {if (keep) {print $0}}' $AWS_CREDENTIALS.bak > ${AWS_CREDENTIALS}
-fi
-
-# add new values
-echo -e "\n[$PROFILE_NAME]" >> ${AWS_CREDENTIALS}
-cat $TEMP_FILE >> ${AWS_CREDENTIALS}
-
-echo "Updated profile $PROFILE_NAME."
-tail -1 ${AWS_CREDENTIALS}
-
 trap cleanup EXIT
 function cleanup() {
-    # remove downmloaded tempfile
-    rm -f ${TEMP_FILE}
-    # kill this chrome
-    kill $(ps ax | grep 'Chrome.app' | grep 'filename=temporary_aws_credentials' | awk '{ print $1; }')
+    (
+        rm -f "${TEMP_FILE}" || true
+        kill "$(pgrep -lf 'Chrome.app' | grep 'temporary_aws_credentials' | awk '{ print $1; }')"
+    ) &>/dev/null
 }
+argv() {
+    arg="${1}"
+    default="${2}"
+    shift; shift
+    echo "${*}" | grep "\-\-${arg}" &>/dev/null \
+        && echo "${*}" | sed -E "s/.*--${arg} ([^ ]*)(.*)?/\1/" \
+        || echo "${default}"
+}
+[[ $# -eq 0 ]] && ( usage && exit 0 )
+PROFILE_NAME="$(argv profile "" "${*:-}")"
+APP_NAME="$(argv app "" "${*:-}")"
+DURATION_HOURS="$(argv duration 4 "${*:-}")"
+ROLE_ARN="$(argv role-arn "" "${*:-}")"
+AWS_CREDENTIALS=~/.aws/credentials
+TEMP_FILE="${HOME}/Downloads/temporary_aws_credentials$(date +"%Y-%m-%d_%H-%M-%S").txt"
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+    --load-extension="${PWD}/chrome_extension" --disable-extensions-except="${PWD}/chrome_extension" \
+    --user-data-dir="${PWD}/user_data" \
+    "http://localhost/?durationHours=${DURATION_HOURS}&app=${APP_NAME}&filename=$(basename "${TEMP_FILE}")&roleArn=${ROLE_ARN}" 2>/dev/null &
+until [ -f "${TEMP_FILE}" ]; do (sleep 1 && echo -n .); done
+echo
+[[ -z "${PROFILE_NAME}" ]] && select _profile in $(sed '/\[profile/!d; s/^\[profile \([^]]*\)\]/\1/' "${HOME}/.aws/config")
+do
+    PROFILE_NAME="${_profile}"
+    break
+done
+awk '/^\[/{keep=1} /^\['"${PROFILE_NAME}"'\]/{keep=0} {if (keep) {print $0}}' ${AWS_CREDENTIALS}.bak > ${AWS_CREDENTIALS}
+echo -e "\n[${PROFILE_NAME}]" >> ${AWS_CREDENTIALS}
+cat "${TEMP_FILE}" >> "${AWS_CREDENTIALS}"
+echo "Updated profile ${PROFILE_NAME}."
