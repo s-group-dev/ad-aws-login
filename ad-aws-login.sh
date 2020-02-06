628f86a5c4f48f1f817a53a128ca9b2f69f41e1a
Jonas Koponen <jonas.koponen@sok.fi>
2020-02-06 13:14:23 +0200

    fix awkward copypaste bugs

--- ad-aws-login.sh
+++ ad-aws-login.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 cd "$(dirname "$0")"
 function usage() {
@@ -30,25 +31,43 @@ argv() {
         && echo "${*}" | sed -E "s/.*--${arg} ([^ ]*)(.*)?/\1/" \
         || echo "${default}"
 }
-[[ $# -eq 0 ]] && ( usage && exit 0 )
+
+readonly AWS_CONFIG="${HOME}/.aws/config"
+
 PROFILE_NAME="$(argv profile "" "${*:-}")"
 APP_NAME="$(argv app "" "${*:-}")"
 DURATION_HOURS="$(argv duration 4 "${*:-}")"
 ROLE_ARN="$(argv role-arn "" "${*:-}")"
 AWS_CREDENTIALS=~/.aws/credentials
 TEMP_FILE="${HOME}/Downloads/temporary_aws_credentials$(date +"%Y-%m-%d_%H-%M-%S").txt"
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
-    --load-extension="${PWD}/chrome_extension" --disable-extensions-except="${PWD}/chrome_extension" \
-    --user-data-dir="${PWD}/user_data" \
-    "http://localhost/?durationHours=${DURATION_HOURS}&app=${APP_NAME}&filename=$(basename "${TEMP_FILE}")&roleArn=${ROLE_ARN}" 2>/dev/null &
-until [ -f "${TEMP_FILE}" ]; do (sleep 1 && echo -n .); done
-echo
+
 [[ -z "${PROFILE_NAME}" ]] && select _profile in $(sed '/\[profile/!d; s/^\[profile \([^]]*\)\]/\1/' "${HOME}/.aws/config")
 do
     PROFILE_NAME="${_profile}"
     break
 done
+
+PROFILE_CONFIG="$(sed -n "/${PROFILE_NAME}/,/^ *$/p" "${AWS_CONFIG}")"
+
+if [[ -z $APP_NAME ]]; then
+    APP_NAME=$(echo "${PROFILE_CONFIG}" | (grep 'app.*' || true) | sed -E 's/^.*app *= *([^ ]*).*$/\1/')
+fi
+
+if [[ -z $ROLE_ARN ]]; then
+    ROLE_ARN=$(echo "${PROFILE_CONFIG}" |  (grep 'role_arn.*' || true) | sed -E 's/^.*role_arn *= *([^ ]*).*$/\1/')
+fi
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+    --load-extension="${PWD}/chrome_extension" --disable-extensions-except="${PWD}/chrome_extension" \
+    --user-data-dir="${PWD}/user_data" \
+    "http://localhost/?durationHours=${DURATION_HOURS}&app=${APP_NAME}&filename=$(basename "${TEMP_FILE}")&roleArn=${ROLE_ARN}" 2>/dev/null &
+
+until [ -f "${TEMP_FILE}" ]; do (sleep 1 && printf "."); done
+
+printf "\n"
+
 awk '/^\[/{keep=1} /^\['"${PROFILE_NAME}"'\]/{keep=0} {if (keep) {print $0}}' ${AWS_CREDENTIALS}.bak > ${AWS_CREDENTIALS}
 echo -e "\n[${PROFILE_NAME}]" >> ${AWS_CREDENTIALS}
 cat "${TEMP_FILE}" >> "${AWS_CREDENTIALS}"
 echo "Updated profile ${PROFILE_NAME}."
+tail -1 ${AWS_CREDENTIALS}
